f12b9862ff7b4dd4c4774b77ae95646a12537405
Patrick Kranz <Pa.Kranz@rz.hochschule-trier.de>
2022-03-14 11:32:36 +0100

    test html2canvas

--- _global/js/general.js
+++ _global/js/general.js
@@ -1,27 +1,25 @@
-function replaceIcon(html)
-{
+function replaceIcon(html) {
   var result = html;
 
   var regEx = new RegExp('(\\{[^\\}]*\\})', "ig");
   var matchs = result.match(regEx);
-  for(var match of (matchs || []))
-  {
+  for (var match of (matchs || [])) {
     var iconName = match.replace('{', '').replace('}', '');
-	var iconNamePieces = iconName.split(",");
-	let elementCount = "";
-	let elementCountText = "";
-	if(iconNamePieces[1]){
-		iconName = iconNamePieces[0];
-		elementCount = iconNamePieces[1];
-		elementCountText += "<div class='element-for-each'><span>"+elementCount+"</span></div>";
-	}
-	
+    var iconNamePieces = iconName.split(",");
+    let elementCount = "";
+    let elementCountText = "";
+    if (iconNamePieces[1]) {
+      iconName = iconNamePieces[0];
+      elementCount = iconNamePieces[1];
+      elementCountText += "<div class='element-for-each'><span>" + elementCount + "</span></div>";
+    }
+
     let iconHtml
-	iconHtml = elementCountText;
-    if(iconName.startsWith('no-')){
-        iconHtml += `<icon class="no ${iconName.substring(3)}"></icon>`;
-    }else{
-        iconHtml += `<icon class="${iconName}"></icon>`;
+    iconHtml = elementCountText;
+    if (iconName.startsWith('no-')) {
+      iconHtml += `<icon class="no ${iconName.substring(3)}"></icon>`;
+    } else {
+      iconHtml += `<icon class="${iconName}"></icon>`;
     }
     result = result.replace(new RegExp(match, "ig"), iconHtml);
   }
@@ -29,3 +27,35 @@ function replaceIcon(html)
   return result;
 }
 
+function downloadURI(uri, name) {
+  var link = document.createElement("a");
+  link.download = name;
+  link.href = uri;
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+  delete link;
+}
+
+window.addEventListener('load', ev => {
+
+  const root = document.body;
+
+  const elements = new Array(...root.childNodes);
+
+  const button = document.createElement('button');
+  button.innerText = "Save"
+  button.addEventListener('click', () => {
+    elements.forEach(async element => {
+      /**
+       * @type HTMLCanvasElement
+       */
+      const canvas = await html2canvas(element);
+      const data = canvas.toDataURL();
+      downloadURI(data, "Spirit.png");
+
+    });
+  })
+
+  root.insertBefore(button, root.firstChild);
+});
