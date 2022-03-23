b7c3f1a0673ce520e420a9914ee9330f161ef245
resonant-gamedesign <44596045+resonant-gamedesign@users.noreply.github.com>
2022-03-23 15:58:02 -0500

    {icon} shortcut in adversary

---
+++ _global/js/adversary.js
@@ -0,0 +1,5 @@
+window.onload = function startMain(){
+    var html = document.querySelectorAll('adversary')[0].innerHTML;
+    document.querySelectorAll('adversary')[0].innerHTML = replaceIcon(html);
+
+}
